be4c04d2ef9f9647f508516b0ef2b04a2fd2e8d3
Romeo RKPK <mindaye8504@gmail.com>
2022-01-31 16:58:36 +0900

    make interface

--- src/main/java/org/lumenk/romeonavigation/routeinfo/RouteInformation.java
+++ src/main/java/org/lumenk/romeonavigation/routeinfo/RouteInformation.java
@@ -1,5 +1,10 @@
 package org.lumenk.romeonavigation.routeinfo;
 
-public interface RouteInformation{
+import org.lumenk.romeonavigation.objects.roads.Road;
+import org.lumenk.romeonavigation.objects.waypoints.Waypoint;
 
+import java.util.ArrayList;
+
+public interface RouteInformation<SegmentInfoType extends SegmentInformation<WaypointIdType, WaypointType, RoadIdType, RoadType>, WaypointIdType, WaypointType extends Waypoint<WaypointIdType>, RoadIdType, RoadType extends Road<WaypointType, WaypointIdType, RoadIdType>> {
+    SegmentInfoType[] toArray();
 }
