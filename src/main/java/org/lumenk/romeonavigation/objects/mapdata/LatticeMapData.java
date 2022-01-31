2e089fe27a327cbf3669ba51c77dbaa26df7a5a2
Romeo RKPK <mindaye8504@gmail.com>
2022-01-31 16:36:12 +0900

    add JavaDoc comments

--- src/main/java/org/lumenk/romeonavigation/objects/mapdata/LatticeMapData.java
+++ src/main/java/org/lumenk/romeonavigation/objects/mapdata/LatticeMapData.java
@@ -1,10 +1,19 @@
 package org.lumenk.romeonavigation.objects.mapdata;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.lumenk.romeonavigation.objects.waypoints.LatticeWaypoint;
 
 import java.util.List;
 
 public interface LatticeMapData<WaypointIdType, WaypointType extends LatticeWaypoint<WaypointIdType>> {
     WaypointType searchById(WaypointIdType id);
+
+    /**
+     * 제시된 지점과 이웃한 모든 지점을 가져옵니다. 이웃의 기준은 LatticeWaypoint에서 정의하지만 그것을 효율적으로 불러오는 부분은 직접 구현하십시오
+     * @param id 제시할 지점
+     * @return 잘못된 id가 주어졌다면 null을 반한하되, 고립된 지점이면 빈 리스트를 반환하십시오
+     */
+    @Nullable
     List<WaypointType> getNeighborhoods(WaypointIdType id);
 }
--- src/main/java/org/lumenk/romeonavigation/objects/waypoints/LatticeWaypoint.java
+++ src/main/java/org/lumenk/romeonavigation/objects/waypoints/LatticeWaypoint.java
@@ -5,6 +5,11 @@ public abstract class LatticeWaypoint<IdType> extends Waypoint<IdType>{
         super(id);
     }
 
+    /**
+     * 다른 객체와 이웃한지 비교합니다. '이웃하였다'의 기준을 직접 정의하십시오
+     * @param other 비교할 대상입니다.
+     * @return 이웃하였으면 true, 그렇지 아니하면 false를 반환합니다
+     */
     public abstract boolean isNeighborhood(LatticeWaypoint<IdType> other);
     public abstract boolean equals(LatticeWaypoint<IdType> other);
 }
