afaf30111f46b86d1c6bd7d0f3c2b0b4929f5683
Romeo RKPK <mindaye8504@gmail.com>
2022-01-31 16:56:15 +0900

    make some abstracts

--- src/main/java/org/lumenk/romeonavigation/routeinfo/SegmentInformation.java
+++ src/main/java/org/lumenk/romeonavigation/routeinfo/SegmentInformation.java
@@ -3,6 +3,32 @@ package org.lumenk.romeonavigation.routeinfo;
 import org.lumenk.romeonavigation.objects.roads.Road;
 import org.lumenk.romeonavigation.objects.waypoints.Waypoint;
 
-public interface SegmentInformation<WaypointIdType, WaypointType extends Waypoint<WaypointIdType>, RoadIdType, RoadType extends Road<WaypointType, WaypointIdType, RoadIdType>> {
+public abstract class SegmentInformation<WaypointIdType, WaypointType extends Waypoint<WaypointIdType>, RoadIdType, RoadType extends Road<WaypointType, WaypointIdType, RoadIdType>> {
+    protected final WaypointType from;
+    protected final WaypointType to;
+    protected final RoadType via;
+    protected final double length;
 
+    protected SegmentInformation(WaypointType from, WaypointType to, RoadType via, double length) {
+        this.from = from;
+        this.to = to;
+        this.via = via;
+        this.length = length;
+    }
+
+    public WaypointType getFrom() {
+        return from;
+    }
+
+    public WaypointType getTo() {
+        return to;
+    }
+
+    public RoadType getVia() {
+        return via;
+    }
+
+    public double getLength() {
+        return length;
+    }
 }
